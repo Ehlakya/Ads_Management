66e8ba75771fdbd2eb1ba554bab99a8ff460d2e1
Ehlakya <ehlakyas@gmail.com>
2026-07-02 10:04:45 +0530

    updates

--- ads_files/backend/config/db.js
+++ ads_files/backend/config/db.js
@@ -6,11 +6,11 @@ dotenv.config();
 const { Sequelize } = require('sequelize');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  }
 });
 
 pool.on('connect', () => {
@@ -22,17 +22,16 @@ pool.on('error', (err) => {
   process.exit(-1);
 });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-    logging: false, // Set to console.log to see SQL queries
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false, // Set to console.log to see SQL queries
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
   }
-);
+});
 
 sequelize.authenticate()
   .then(() => console.log('Sequelize connected successfully'))
